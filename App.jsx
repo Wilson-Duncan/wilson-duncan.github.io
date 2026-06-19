f3e2a1fa7fc5f2a563928692843398cf039255c6
Wilson-Duncan <wildun.dwd@gmail.com>
2026-06-18 21:57:46 -0400

    Refactored to use react, added dependencies and jsx file

---
+++ App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+  <>
+    <h1>Test Github page</h1>
+    <p>Site content and background</p>
+  </>
+  );
+}
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(<App />);
